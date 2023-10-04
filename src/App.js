c78e4c763209963d5bfdc63114d9a3f153feaff5
michaelvb-24 <michael.vanbouchotte1@gmail.com>
2023-10-04 13:13:17 +0200

    scroll fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import { Suspense } from "react";
-import { Outlet } from "react-router-dom";
+import { Suspense, useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import "./App.css";
 import AuthProvider from "./components/AuthProvider/AuthProvider";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
 function App() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
   return (
     <div className="App">
       <AuthProvider>
